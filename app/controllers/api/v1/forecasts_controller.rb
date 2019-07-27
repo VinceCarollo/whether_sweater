1fed2ef9e27fe3399c8428c13b90d3094d3beb0d
Vince Carollo <vincecarollo@gmail.com>
2019-07-27 14:20:53 -0600

    Refactor api v1 forecasts controller to us city_weather helper method

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -2,13 +2,19 @@ class Api::V1::ForecastsController < ApplicationController
   def index
     begin
       location = params[:location]
-      forecast_data = WeatherService.forecast_data(location)
-      forecast = Forecast.new(forecast_data, location)
-      forecast.add_main_data
-      weather_for_city = ForecastSerializer.new(forecast).serializable_hash
-      render json: weather_for_city
+      city_weather = city_weather(location)
+      render json: city_weather
     rescue NoMethodError
       raise ActionController::RoutingError.new('City is Invalid')
     end
+  end
+
+  private
+
+  def city_weather(location)
+    forecast_data = WeatherService.forecast_data(location)
+    forecast = Forecast.new(forecast_data, location)
+    forecast.add_main_data
+    ForecastSerializer.new(forecast).serializable_hash
   end
 end
