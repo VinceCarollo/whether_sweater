abdef62178800942c8d059175f5c5ecf94fd3da3
Vince Carollo <vincecarollo@gmail.com>
2019-07-27 12:39:26 -0600

    Add both google location and darksky fetch, start main forecast box endpoint

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::ForecastsController < ApplicationController
   def index
-    
+    response = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |req|
+      req.params[:address] = params[:location]
+      req.params[:key] = ENV['GOOGLE_API_KEY']
+    end
+    location_data = JSON.parse(response.body, symbolize_names: true)
+    lat = location_data[:results].first[:geometry][:location][:lat]
+    lng = location_data[:results].first[:geometry][:location][:lng]
+    response = Faraday.get("https://api.darksky.net/forecast/#{ENV['DARKSKY_API_KEY']}/#{lat},#{lng}")
+    binding.pry
   end
 end
