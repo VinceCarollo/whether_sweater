02baf3b031cc38624f56a9e9a35df7e8553228f5
Vince Carollo <vincecarollo@gmail.com>
2019-07-27 15:21:42 -0600

    Migrate empty schema

--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -20,6 +20,23 @@ RSpec.describe "Forecast API" do
       expect(main).to have_key(:icon)
     end
 
+    it "returns main city info from different given city" do
+      get '/api/v1/forecast?location=kansas city,mo'
+
+      main = JSON.parse(response.body, symbolize_names: true)[:data][:attributes][:city_weather][:main]
+
+      expect(main).to have_key(:city)
+      expect(main[:city]).to eq('kansas city, mo')
+      expect(main).to have_key(:summary)
+      expect(main).to have_key(:temperature)
+      expect(main).to have_key(:high)
+      expect(main).to have_key(:low)
+      expect(main).to have_key(:country)
+      expect(main).to have_key(:time)
+      expect(main).to have_key(:date)
+      expect(main).to have_key(:icon)
+    end
+
     it "returns 404 if invalid city" do
       expect{ get '/api/v1/forecast?location=invalid' }.to raise_error(ActionController::RoutingError)
     end
@@ -39,6 +56,12 @@ RSpec.describe "Forecast API" do
       expect(details).to have_key(:humidity)
       expect(details).to have_key(:visibility)
       expect(details).to have_key(:uv_index)
+    end
+  end
+
+  context 'extended forecast section' do
+    it "returns extended forecast from given city" do
+
     end
   end
 end
