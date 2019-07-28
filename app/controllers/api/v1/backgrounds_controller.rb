41dbfa6567d3981a7a27b97dbfc69418e2fe38f3
Vince Carollo <vincecarollo@gmail.com>
2019-07-27 20:28:34 -0600

    Add background enpoint that grabs 10 image backgrounds based on location input

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,5 +1,18 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index
-    response = Faraday.get('https://api.unsplash.com/search/photos?query=denver&client_id=dd3e9a5e6d4d4e3c910754795ec90924d032421cf988c97280e021e4fe49f0b1')
+    query = params[:location].split(',').first
+    response = Faraday.get('https://api.unsplash.com/search/photos') do |req|
+      req.params[:query] = query
+      req.params[:client_id] = ENV['UNSPLASH_API_KEY']
+    end
+    images_data = JSON.parse(response.body, symbolize_names: true)
+    images = Array.new
+    images_data[:results][0..9].each do |image_data|
+      images << {
+        url: image_data[:urls][:full],
+        alt_description: image_data[:alt_description]
+      }
+    end
+    render json: { data: images }
   end
 end
--- spec/requests/api/v1/background_img_spec.rb
+++ spec/requests/api/v1/background_img_spec.rb
@@ -9,8 +9,21 @@ RSpec.describe "Background API" do
 
     backgrounds = JSON.parse(response.body, symbolize_names: true)
 
-    expect(backgrounds[:images]).to be_a Array
-    expect(backgrounds[:images].count).to be > 1
-    expect(backgrounds[:images].first).to have_key(:url)
+    expect(backgrounds[:data]).to be_a Array
+    expect(backgrounds[:data].count).to eq(10)
+    expect(backgrounds[:data].first).to have_key(:url)
+  end
+
+  it "gets a background image based on different location input" do
+
+    get '/api/v1/backgrounds?location=kansas city,mo'
+
+    expect(response).to be_successful
+
+    backgrounds = JSON.parse(response.body, symbolize_names: true)
+
+    expect(backgrounds[:data]).to be_a Array
+    expect(backgrounds[:data].count).to eq(10)
+    expect(backgrounds[:data].first).to have_key(:url)
   end
 end
