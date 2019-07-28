18174c008372f541a0be6edc3bc326d0985964b3
Vince Carollo <vincecarollo@gmail.com>
2019-07-28 16:54:27 -0600

    Remove uncecessary include statement in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 require 'api_key'
 
 class User < ApplicationRecord
-  include ApiKey
   validates :email, uniqueness: true, presence: true
   validates_presence_of :api_key, :password
   has_secure_password
