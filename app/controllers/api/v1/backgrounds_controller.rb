2265656d2be9ecadff76accde4b30952e171dd96
Vince Carollo <vincecarollo@gmail.com>
2019-07-27 20:36:20 -0600

    refactor api v1 backgrounds controller to use create_image helper

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -2,13 +2,18 @@ class Api::V1::BackgroundsController < ApplicationController
   def index
     city = params[:location].split(',').first
     images_data = ImageService.backgrounds(city)
-    images = Array.new
-    images_data[:results][0..9].each do |image_data|
-      images << {
+    images = create_images(images_data)
+    render json: { data: images }
+  end
+
+  private
+
+  def create_images(images_data)
+    images_data[:results][0..9].map do |image_data|
+      {
         url: image_data[:urls][:full],
         alt_description: image_data[:alt_description]
       }
     end
-    render json: { data: images }
   end
 end
