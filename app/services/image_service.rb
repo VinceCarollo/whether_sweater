98089b8a3a92754ca4ba60cf2b8d5f1358905830
Vince Carollo <vincecarollo@gmail.com>
2019-07-27 20:43:14 -0600

    refactor images_service to use image_request helper

--- app/services/image_service.rb
+++ app/services/image_service.rb
@@ -1,9 +1,15 @@
 class ImageService
   def self.backgrounds(city)
-    response = Faraday.get('https://api.unsplash.com/search/photos') do |req|
+    response = image_request(city)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def self.image_request(city)
+    Faraday.get('https://api.unsplash.com/search/photos') do |req|
       req.params[:query] = city
       req.params[:client_id] = ENV['UNSPLASH_API_KEY']
     end
-    JSON.parse(response.body, symbolize_names: true)
   end
 end
