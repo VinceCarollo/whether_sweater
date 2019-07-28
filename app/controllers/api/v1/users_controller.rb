fa78f7a837270e4820f0e8505c41ecedca929f16
Vince Carollo <vincecarollo@gmail.com>
2019-07-28 16:42:13 -0600

    Refactor api v1 users controller create action to use helpers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,24 +2,31 @@ class Api::V1::UsersController < ApplicationController
   protect_from_forgery with: :null_session
 
   def create
-    response = {}
     user = User.new(user_params)
     if user.save
-      render json: {
-        status: 201,
-        body: {
-          api_key: user.api_key
-        }
-      }
+      render_successful(user)
     else
-      render json: {
-        status: 409,
-        errors: user.errors.full_messages
-      }, status: 409
+      render_errors(user)
     end
   end
 
   private
+
+  def render_successful(user)
+    render json: {
+      status: 201,
+      body: {
+        api_key: user.api_key
+      }
+    }
+  end
+
+  def render_errors(user)
+    render json: {
+      status: 409,
+      errors: user.errors.full_messages
+    }, status: 409
+  end
 
   def user_params
     params.permit(:email, :password, :password_confirmation)
