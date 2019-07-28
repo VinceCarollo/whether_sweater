9e6a08d2ea9a7f9e07023d362546258ac38cde9a
Vince Carollo <vincecarollo@gmail.com>
2019-07-28 16:43:18 -0600

    Add session creation to user creation through api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      session[:user_id] = user.id
       render_successful(user)
     else
       render_errors(user)
