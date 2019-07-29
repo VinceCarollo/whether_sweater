2c6f089a6d78b635e2a1fa0b85a9f339a455fef3
Vince Carollo <vincecarollo@gmail.com>
2019-07-29 11:33:20 -0600

    Add google service spec

--- app/models/munchie.rb
+++ app/models/munchie.rb
@@ -8,7 +8,7 @@ class Munchie
   private
 
   def add_travel_data(data)
-    @origin = data[:routes].first[:legs].first[:start_address]
+    @origin =      data[:routes].first[:legs].first[:start_address]
     @destination = data[:routes].first[:legs].first[:end_address]
     @travel_time = data[:routes].first[:legs].first[:duration][:text]
   end
@@ -16,10 +16,10 @@ class Munchie
   def add_restaurant_data(data)
     @restaurants = data[:businesses][0..2].map do |business|
       {
-      name: business[:name],
+      name:           business[:name],
       street_address: business[:location][:address1],
-      city: business[:location][:city],
-      zip: business[:location][:zip_code]
+      city:           business[:location][:city],
+      zip:            business[:location][:zip_code]
       }
     end
   end
