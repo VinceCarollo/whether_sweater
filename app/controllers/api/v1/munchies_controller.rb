40dbdfc36ffe535e4403aa199599fa466aecfa69
Vince Carollo <vincecarollo@gmail.com>
2019-07-29 10:03:13 -0600

    Add farady yelp fetch to munchies index action

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
     travel_data = travel_time_service
-    
+    response = Faraday.get('https://api.yelp.com/v3/businesses/search') do |req|
+      req.headers['Authorization'] = 'Bearer mX5uLjmCuUsm-nG3bCKU2bmNFN_fca-qLTju6LRSXgCQmFqfBZDF3IROzkpv_r4ID5RcoCFrjE4CEC_TVJIEpMXrzYMLUJ8J-ipFm5EffcQTm_YH_Rlg2_fptRY_XXYx'
+      req.params['location'] = params[:end]
+      req.params['term'] = params[:food]
+    end
+    binding.pry
   end
 
   private
