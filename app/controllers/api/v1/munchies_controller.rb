e01c17aed77a2868f5afbdcf5082f5e9abd15be4
Vince Carollo <vincecarollo@gmail.com>
2019-07-29 09:43:06 -0600

    Add fetching of travel time to munchies controller

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
-    binding.pry
+    travel_data = travel_time_service
+    
+  end
+
+  private
+
+  def travel_time_service
+    start_city = params[:start]
+    end_city = params[:end]
+    GoogleService.travel_time(start_city, end_city)
   end
 end
--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -8,6 +8,17 @@ class GoogleService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def self.travel_time(start_city, end_city)
+    response = conn.get('/maps/api/directions/json') do |req|
+      req.params[:origin] = start_city
+      req.params[:destination] = end_city
+      req.params[:key] = ENV['GOOGLE_API_KEY']
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
   def self.conn
     Faraday.new('https://maps.googleapis.com')
   end
