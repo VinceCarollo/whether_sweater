5fe90a4fa5c750958617884a746a5133fd490cc7
Vince Carollo <vincecarollo@gmail.com>
2019-07-29 17:59:26 -0600

    Add munchies facade

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,34 +1,9 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
     begin
-      travel_data = travel_time_service
-      restaurant_data = restaurant_service(travel_data)
-      munchie = initiate_munchie(travel_data, restaurant_data)
-      render json: { data: munchie }
+      render json: MunchiesFacade.munchie_data(params)
     rescue NoMethodError
       render json: { status: 404, description: "Not Found"}, status: 404
     end
-  end
-
-  private
-
-  def initiate_munchie(travel_data, restaurant_data)
-    munchie = Munchie.new
-    munchie.add_data(travel_data, restaurant_data)
-    munchie
-  end
-
-  def restaurant_service(travel_data)
-    travel_time = travel_data[:routes].first[:legs].first[:duration][:value]
-    hours_traveled = Time.at(travel_time).utc.hour
-    minutes_traveled = Time.at(travel_time).utc.min
-    time_arriving = Time.now + hours_traveled.hours + minutes_traveled.minutes
-    YelpService.open_restaurants(time_arriving, params[:end], params[:food])
-  end
-
-  def travel_time_service
-    start_city = params[:start]
-    end_city = params[:end]
-    GoogleService.travel_time(start_city, end_city)
   end
 end
---
+++ app/facades/munchies_facade.rb
@@ -0,0 +1,32 @@
+class MunchiesFacade
+  def self.munchie_data(params)
+    @params = params
+    travel_data = travel_time_service
+    restaurant_data = restaurant_service(travel_data)
+    munchie = initiate_munchie(travel_data, restaurant_data)
+    { data: munchie }
+  end
+
+  private
+
+  def self.initiate_munchie(travel_data, restaurant_data)
+    munchie = Munchie.new
+    munchie.add_data(travel_data, restaurant_data)
+    munchie
+  end
+
+  def self.restaurant_service(travel_data)
+    travel_time = travel_data[:routes].first[:legs].first[:duration][:value]
+    hours_traveled = Time.at(travel_time).utc.hour
+    minutes_traveled = Time.at(travel_time).utc.min
+    time_arriving = Time.now + hours_traveled.hours + minutes_traveled.minutes
+    YelpService.open_restaurants(time_arriving, @params[:end], @params[:food])
+  end
+
+  def self.travel_time_service
+    start_city = @params[:start]
+    end_city = @params[:end]
+    GoogleService.travel_time(start_city, end_city)
+  end
+
+end
