41c84ebdabe02235c4ca470ef86de0d2a9803dd7
Vince Carollo <vincecarollo@gmail.com>
2019-07-29 11:24:51 -0600

    Add to munchie request spec

--- spec/requests/api/v1/munchies_spec.rb
+++ spec/requests/api/v1/munchies_spec.rb
@@ -21,6 +21,26 @@ RSpec.describe 'Munchies API' do
     expect(travel_data[:restaurants].first).to have_key(:zip)
   end
 
+  it "can get time to travel and restaurants upon arrival to different city" do
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+
+    get '/api/v1/munchies?start=denver,co&end=kansas city,mo&food=bbq', headers: headers
+
+    expect(response).to be_successful
+
+    travel_data = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(travel_data).to have_key(:origin)
+    expect(travel_data).to have_key(:destination)
+    expect(travel_data).to have_key(:travel_time)
+    expect(travel_data[:restaurants]).to be_a Array
+    expect(travel_data[:restaurants].count).to eq(3)
+    expect(travel_data[:restaurants].first).to have_key(:name)
+    expect(travel_data[:restaurants].first).to have_key(:street_address)
+    expect(travel_data[:restaurants].first).to have_key(:city)
+    expect(travel_data[:restaurants].first).to have_key(:zip)
+  end
+
   it "gives 404 for bad input" do
     headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 
