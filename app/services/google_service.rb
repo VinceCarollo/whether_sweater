5dd1aec67700f8f06dee487bbcbbca313aa89e85
Vince Carollo <vincecarollo@gmail.com>
2019-07-29 09:34:01 -0600

    Refactor google service for reusability

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -1,10 +1,14 @@
 class GoogleService
 
   def self.location_data(location)
-    response = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |req|
+    response = conn.get("/maps/api/geocode/json?address=#{location}") do |req|
       req.params[:address] = location
       req.params[:key] = ENV['GOOGLE_API_KEY']
     end
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.conn
+    Faraday.new('https://maps.googleapis.com')
   end
 end
