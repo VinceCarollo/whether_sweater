f854cdcc8c7796254777a0e346abfbcaef047561
Vince Carollo <vincecarollo@gmail.com>
2019-07-30 14:34:34 -0600

    Add users base controller and users road trips controller for road trips endpoint

--- app/controllers/api/v1/users/base_controller.rb
+++ app/controllers/api/v1/users/base_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::Users::BaseController < ApplicationController
+  protect_from_forgery with: :null_session
   before_action :require_user
 
   def require_user
--- app/controllers/api/v1/users/road_trips_controller.rb
+++ app/controllers/api/v1/users/road_trips_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::Users::RoadTripsController < Api::V1::Users::BaseController
+  protect_from_forgery with: :null_session
+  
   def index
     origin = params[:origin]
     destination = params[:destination]
