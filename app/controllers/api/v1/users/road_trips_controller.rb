3aeef4854b0f108b82fdeecc073f1244cf879fb5
Vince Carollo <vincecarollo@gmail.com>
2019-07-30 14:48:35 -0600

    Add road trip facade

--- app/controllers/api/v1/users/road_trips_controller.rb
+++ app/controllers/api/v1/users/road_trips_controller.rb
@@ -1,12 +1,7 @@
 class Api::V1::Users::RoadTripsController < Api::V1::Users::BaseController
   protect_from_forgery with: :null_session
-  
+
   def index
-    origin = params[:origin]
-    destination = params[:destination]
-    travel_data = GoogleService.travel_time(origin, destination)
-    weather_data = WeatherService.forecast_data(destination)
-    road_trip = RoadTrip.new(travel_data, weather_data)
-    render json: RoadTripSerializer.new(road_trip)
+    render json: RoadTripFacade.trip(params)
   end
 end
